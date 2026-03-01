dd36b4f3c7e6d826e2657b20b1a6afb866566eb8
Никита <Goldbergnik1993@gmail.com>
2026-03-01 19:36:38 +0100

    dev3

--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$location = "northeurope"
+$location = "uksouth"
 $resourceGroupName = "mate-azure-task-9"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
@@ -8,15 +8,12 @@ $subnetAddressPrefix = "10.0.0.0/24"
 $publicIpAddressName = "linuxboxpip"
 $sshKeyName = "linuxboxsshkey"
 $vmName = "matebox"
-$vmImage = "Ubuntu2204"
-$vmSize = "Standard_D2s_v3"
+$vmSize = "Standard_B1s"
 
-$sshKeyPath = "$HOME/.ssh/id_ed25519.pub"
-$sshKeyPublicKey = Get-Content $sshKeyPath
+$sshKeyPublicKey = Get-Content "$HOME/.ssh/id_ed25519.pub"
 
 New-AzResourceGroup -Name $resourceGroupName -Location $location -Force
 
-# 1. Создание ресурса SSH ключа в Azure (требование задачи)
 $sshKey = New-AzSshKey -ResourceGroupName $resourceGroupName -Name $sshKeyName -Location $location -PublicKey $sshKeyPublicKey
 
 $nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow
@@ -26,7 +23,6 @@ $nsg = New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroup
 $subnetConfig = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
 $vnet = New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnetConfig
 
-# 2. Создание IP с обязательным DNS Label
 $dnsLabel = "matebox-nikita-$(Get-Random -Minimum 1000 -Maximum 9999)"
 $publicIp = New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -AllocationMethod Static -Sku Standard -DomainNameLabel $dnsLabel
 
@@ -35,14 +31,11 @@ $nic = New-AzNetworkInterface -Name "nic-$vmName" -ResourceGroupName $resourceGr
 $securePassword = ConvertTo-SecureString "TempPass123!" -AsPlainText -Force
 $cred = New-Object System.Management.Automation.PSCredential ("azureuser", $securePassword)
 
-# 3. Конфигурация ВМ с привязкой к ресурсу SSH ключа
 $vmConfig = New-AzVMConfig -VMName $vmName -VMSize $vmSize
 $vmConfig = Set-AzVMOperatingSystem -VM $vmConfig -Linux -ComputerName $vmName -Credential $cred
-$vmConfig = Set-AzVMSourceImage -VM $vmConfig -ImageName $vmImage
+$vmConfig = Set-AzVMSourceImage -VM $vmConfig -PublisherName "Canonical" -Offer "0001-com-ubuntu-server-jammy" -Skus "22_04-lts" -Version "latest"
 $vmConfig = Add-AzVMNetworkInterface -VM $vmConfig -Id $nic.Id
 
-# Привязываем созданный ресурс SSH ключа по его ID
-$vmConfig = Add-AzVMSshPublicKey -VM $vmConfig -KeyData $sshKeyPublicKey -Path "/home/azureuser/.ssh/authorized_keys"
-
-New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig
+# Финальное создание ВМ с привязкой SSH-ключа через -SshKeyName
+New-AzVM -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig -SshKeyName $sshKeyName
 
